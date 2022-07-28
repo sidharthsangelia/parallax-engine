ed9ddf2e2d08a7fd414331d8ea71438be4b149ad
sidharthsangelia <106264907+sidharthsangelia@users.noreply.github.com>
2022-07-29 00:14:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Buy from './components/Buy'
+import Carousel from './components/Carousel'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Carousel/>
+    <Buy/>
+    <Contact/>
+    <Footer/>
+    </>
+
+  )
+}
+
+export default App
